0f471265a3949000472642dd2d82b0d885087170
stephonduong <stephonduong@gmail.com>
2022-08-03 19:48:23 -0400

    finished login header feature

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -8,7 +8,6 @@ const API_URL = process.env.REACT_APP_API_URL;
 
 const Header = () => {
   const [ isShowing, setIsShowing ] = useState(false)
-  // const [ isRegisterShowing, setIsRegisterShowing ] = useState(false)
   const [ isLoggedIn, setIsLoggedIn] = useState(false)
   const [ currentUser, setCurrentUser ] = useState(null)
 
@@ -16,16 +15,11 @@ const Header = () => {
     setIsShowing(!isShowing)
   }
 
-  // const toggleRegisterModal = () => {
-  //   setIsRegisterShowing(!isRegisterShowing)
-  // }
-
   useEffect(() => {
     const authToken = sessionStorage.getItem('authToken')
     if (!authToken) {
       setIsLoggedIn(false)
-    } else {
-      console.log(isShowing)
+    } else if (!isLoggedIn) {
       axios
         .get(`${API_URL}/users/current`, {
           headers: {
@@ -40,7 +34,7 @@ const Header = () => {
           setIsLoggedIn(false)
         });
       }}
-  , []);
+    );
 
   const handleLogout = () => {
     setCurrentUser(null);
@@ -48,24 +42,23 @@ const Header = () => {
     sessionStorage.removeItem('authToken')
   }
 
+  console.log(currentUser)
+
   if (!isLoggedIn) {
     return (
-      
       <div className='header'>
         <h2 className='header__logo'>Read.ME</h2>
         <form className='header__form'>
         <input className='header__search-bar' placeholder='Search'/>
         </form>
-        <div className='header__buttons'>
+        <div className='header__buttons-section'>
           <button className='header__button' onClick={toggleModal}>Login</button>
           <button className='header__button'>Go To Stories</button>
         </div>
         <LoginModal isShowing={isShowing} hide={toggleModal} setIsLoggedIn={setIsLoggedIn}/>
       </div>
     );
-  };
-  
-  if (isLoggedIn) {
+  } else {
   return (
     <div className='header'>
       <h2 className='header__logo'>Read.ME</h2>
--- src/components/LoginModal/LoginModal.js
+++ src/components/LoginModal/LoginModal.js
@@ -4,12 +4,11 @@ import './LoginModal.scss';
 
 const API_URL = process.env.REACT_APP_API_URL;
 
-const LoginModal = ({isShowing, hide, setIsLoggedIn}) => {
-  const [ loginError, setLoginError ] = useState('')
-  const [ loginSuccess, setLoginSuccess ] = useState(false)
+const LoginModal = ({isShowing, hide}) => {
+  const [ isError, setIsError ] = useState(false)
   const [ isRegister, setIsRegister] = useState(false)
 
-  function handleSubmit(event) {
+  const handleLogin = (event) => {
     event.preventDefault();
 
     const form = event.target;
@@ -22,13 +21,36 @@ const LoginModal = ({isShowing, hide, setIsLoggedIn}) => {
     axios.post(`${API_URL}/users/login`, loginInfo)
       .then((response) => {
         sessionStorage.setItem('authToken', response.data.token);
-        setLoginSuccess(true);
-        hide()
-        setIsLoggedIn(true)
+        setIsError(false);
+        hide();
       })
       .catch(err => {
-        setLoginError(err.response.data)
+        setIsError(true);
       })
+  }
+
+  const handleRegister = (event) => {
+    event.preventDefault();
+
+    const form = event.target;
+
+    const userInfo = {
+      first_name: form.first_name.value,
+      last_name: form.last_name.value,
+      email: form.email.value,
+      username: form.username.value,
+      password: form.password.value
+    }
+
+    axios
+      .post(`${API_URL}/users/register`, userInfo)
+        .then((res) => {
+          setIsRegister(false);
+          setIsError(false);
+        })
+        .catch(err => {
+          setIsError(true);
+        })
   }
 
   return (
@@ -43,26 +65,35 @@ const LoginModal = ({isShowing, hide, setIsLoggedIn}) => {
                 <span aria-hidden="true">&times;</span>
               </button>
             </div>
-            <form className='login__form' onSubmit={handleSubmit}>
+            <form className='login__form' onSubmit={handleRegister} >
+              <div>
+                <div>
+                  <label className="login__label" htmlFor="text">First Name</label>
+                  <input className="login__input" type="text" name='first_name'/>
+                </div>
+                <div>
+                  <label className="login__label" htmlFor="text">Last Name</label>
+                  <input className="login__input" type="text" name='last_name'/>
+              </div>
+              </div>
               <label className="login__label" htmlFor="email">E-mail</label>
               <input className="login__input" type="email" name='email'/>
-              <label className="login__label" htmlFor="email">Name</label>
-              <input className="login__input" type="email" name='email'/>
+              <label className="login__label" htmlFor="text">Username</label>
+              <input className="login__input" type="text" name='username'/>
               <label className="login__label" htmlFor="password">Password</label>
               <input className="login__input" type="password" name='password'/>
               <button className="login__button">Log in</button>
               
               {/* Error message */}
-              {loginError && (
-                <p>Incorrect Email and Password</p>
+              {isError && (
+                <p>Please fill in all fields correctly</p>
               )}
             </form>
-            <p>Need an account? <span onClick={() => {setIsRegister(false)}}>Login here</span></p>
+            <p>Already have an account? <span onClick={() => {setIsRegister(false)}} className="link">Login here</span></p>
           </div>
         </div>
         </div> 
         : 
-
         <div className="login">
         <div className="login__modal-overlay"/>
         <div className="login__modal-wrapper" aria-modal aria-hidden tabIndex={-1} role="dialog">
@@ -72,7 +103,7 @@ const LoginModal = ({isShowing, hide, setIsLoggedIn}) => {
                 <span aria-hidden="true">&times;</span>
               </button>
             </div>
-            <form className='login__form' onSubmit={handleSubmit}>
+            <form className='login__form' onSubmit={handleLogin}>
               <label className="login__label" htmlFor="email">E-mail</label>
               <input className="login__input" type="email" name='email'/>
               <label className="login__label" htmlFor="password">Password</label>
@@ -80,11 +111,11 @@ const LoginModal = ({isShowing, hide, setIsLoggedIn}) => {
               <button className="login__button">Log in</button>
               
               {/* Error message */}
-              {loginError && (
+              {isError && (
                 <p>Incorrect Email and Password</p>
               )}
             </form>
-            <p>Need an account? <span onClick={() => {setIsRegister(true)}}>Register here</span></p>
+            <p>Need an account? <span onClick={() => {setIsRegister(true)}} className="link">Register here</span></p>
           </div>
         </div>
         </div>) : null)
