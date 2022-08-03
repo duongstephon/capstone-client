a2f517fdca70cdcbcc19e464ac5198de7976714e
stephonduong <stephonduong@gmail.com>
2022-08-03 15:56:33 -0400

    added login-features

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.scss';
 import Header from './components/Header/Header';
+import MainPage from './pages/MainPage/MainPage';
 
 function App() {
   return (
     <div>
     <Header />
+    <MainPage />
     </div>
   );
 }
--- src/pages/MainPage/MainPage.js
+++ src/pages/MainPage/MainPage.js
@@ -1,11 +1,45 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useState, useEffect } from 'react';
+import './MainPage.scss'
+
+const API_URL = process.env.REACT_APP_API_URL;
 
 const MainPage = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  const [ posts, setPosts ] = useState([])
+  const [ isLoggedIn, setIsLoggedIn ] = useState(false)
+  const [ user, setUser ] = useState(null)
+
+  const handleShuffle = (array) => {
+    let currIndex = array.length, randomIndex;
+    while (currIndex !== 0) {
+      randomIndex = Math.floor(Math.random() * currIndex);
+      currIndex--;
+
+      [array[currIndex], array[randomIndex]] = [array[randomIndex], array[currIndex]]
+    }
+    return array
+  }
+
+  // useEffect(() => {
+  //   const authToken = sessionStorage.getItem('authToken');
+  //   if (!authToken) {
+  //     setIsLoggedIn(false)
+  //   }
+
+  //   axios
+  //     .get(`${API_URL}/posts`)
+  //     .then(response => {
+  //       setPosts(handleShuffle(response.data))
+  //     })
+  // })
+
+  return (
+    <div>
+        {posts.map(post => {
+          <p>{post.id}</p>
+        })}
+    </div>
+  );
 };
 
 export default MainPage;
